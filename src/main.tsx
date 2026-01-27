60f69c8fcd638d72d133aaed74d9f49f132ae7e0
Nikita A. <artamnikita@yandex.ru>
2026-01-27 13:09:58 +0300

    Upgrade

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import '@coinbase/onchainkit/styles.css'
-import './index.css'
 import App from './App'
 
 import { 
