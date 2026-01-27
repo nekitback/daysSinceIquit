0b91fcbd422b73612cfde99e0f299a1d0db02241
Nikita A. <artamnikita@yandex.ru>
2026-01-27 23:51:19 +0300

    Upgrade

---
+++ frame/page.tsx
@@ -0,0 +1,10 @@
+import { Frame } from "frames.js/next";
+
+export default function Page() {
+  return (
+    <Frame
+      url="https://days-since-iquit.vercel.app/api/frame"
+      title="Days Since I Quit"
+    />
+  );
+}
